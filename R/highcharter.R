d2a94dbefc19351dbc5a77d4fda43d608a55e1ab
Jaehyeon Kim <dottami@gmail.com>
2017-10-09 10:05:52 +0000

    leaflet example

--- R/highcharter.R
+++ R/highcharter.R
@@ -60,3 +60,9 @@ p <- plot_ly(data, x = ~x) %>%
     add_trace(y = ~trace_2, name = 'trace 2', mode = 'markers')
 
 
+library(leaflet)
+
+m <- leaflet() %>%
+    addTiles() %>%  # Add default OpenStreetMap map tiles
+    addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
+m  # Print the map
